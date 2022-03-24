fe90e99fc09eb374c007f486ee0903977cfe321e
Irina Lavrinenko <82973772+IrinaL1@users.noreply.github.com>
2022-03-24 22:21:43 +0300

    Update flight_tp.h

--- flight_tp.h
+++ flight_tp.h
@@ -13,8 +13,6 @@ class FlightBetweenTwoPoints: public Flight {
 	public:
 		FlightBetweenTwoPoints * return_flight(int return_start_day, int return_start_time) const;
 		using Flight::Flight;
-//		FlightBetweenTwoPoints(string * new_Points, struct time * new_Time, int Num_P = 2): 
-//			Flight(2, new_Points, new_Time) {}
 
 };
 
