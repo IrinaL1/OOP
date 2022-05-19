b5ca1607fe0a83219bde45c917ce800ccd7a9537
Irina Lavrinenko <82973772+IrinaL1@users.noreply.github.com>
2022-05-20 01:57:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ lib = ctypes.CDLL('./lib.so')
 lib.create_timetable.argtypes = [ctypes.c_char_p]
 lib.create_timetable.restype = ctypes.c_void_p
 lib.print_timetable.argtypes = [ctypes.c_void_p]
-a = ctypes.create_string_buffer(str.encode('./airports.txt'))
+filename = input ("Введите название файла: ")
+a = ctypes.create_string_buffer(str.encode(filename))
 t = lib.create_timetable(a)
 lib.print_timetable(t)
