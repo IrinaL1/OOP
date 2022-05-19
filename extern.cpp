577ed81bd618cc5e09d85d70638e5191a0bb1dc7
Irina Lavrinenko <82973772+IrinaL1@users.noreply.github.com>
2022-05-20 00:28:32 +0300

    Add files via upload

--- extern.cpp
+++ extern.cpp
@@ -10,21 +10,32 @@
 #include "flight.h"
 #include "cyclic_flight.h"
 
-extern "C" {
-	Timetable * create_timetable (char * filename) {
 
-		ifstream file_ptr;
-		file_ptr.open(filename);
+extern "C" Timetable * create_timetable(char * filename) {
+		cout << "Start" << '\n';
+		ifstream file_ptr(filename);
+		//file_ptr.open(filename);
 		if (!file_ptr) cout << "No such file" << '\n';
-		
+		cout << "Start" << '\n';
 		Timetable * T = new Timetable;
 		//считываю названия эропортов
 		std::string buf;
+		std::string buf_1;
 		std::vector<std::string> FilePoints;
-		while (file_ptr.peek() != ';' && std::getline(file_ptr, buf)){
+		cout << "Start" << '\n';
+		if(file_ptr.is_open()) cout << "Open" << '\n';
+		int i = 0;
+		bool b;
+		while (std::getline(file_ptr, buf)) {
+			if(buf == ";") break;
 			FilePoints.push_back(buf);
+			cout << "in while" << '\n';
+			cout << i << '\n';
+			cout << buf;
+			cout << b << '\n';
+			i++;
 		}
-		std::getline(file_ptr, buf); //считываю разделитель
+		if(!std::getline(file_ptr, buf)) cout << "Error reading" << '\n'; //считываю разделитель
 
 		//создаю аэропорты-объекты
 		std::map<std::string, Point> airports;
@@ -35,28 +46,29 @@ extern "C" {
 		std::map <std::string, std::pair<int, int>> bound_time;
 
 		while(file_ptr.peek() != '.') {
+			if(!std::getline(file_ptr, name_1)) cout << name_1 << '\n';
 			while(file_ptr.peek() != ';' && std::getline(file_ptr, name_2)) {
 				if(!std::getline(file_ptr, buf)) 
-					cout << "Error reading" << '\n';
+					cout << "Error reading min_time airports" << '\n';
 				else{
 					min_time = stoi(buf);
 					if(min_time <= 20)
 						cout << "Incorrect min_time" << '\n';
 				}
 				if(!std::getline(file_ptr, buf)) 
-					cout << "Error reading" << '\n';
+					cout << "Error reading max_time airports" << '\n';
 				else{
 					max_time = stoi(buf);
 					if(max_time <= 20 || max_time < min_time)
 						cout << "Incorrect max_time" << '\n';
 				}
 				bound_time.emplace(name_2, make_pair(min_time, max_time));			
-				}
+			}
 				airports.emplace(name_1, Point(name_1, bound_time));
 		}
 		
 		if(!std::getline(file_ptr, buf)) cout << "Error reading" << '\n'; //считываю разделитель
-		
+		cout << "After airports" << '\n';
 		//задаю рейсы
 		//name_1 - город отправления
 		//name_2 - буфер, в который считываю следующие города в рейсе
@@ -96,16 +108,15 @@ extern "C" {
 			}
 			if(!std::getline(file_ptr, buf)) cout << "Error reading or End of file" << '\n'; //считываю разделитель
 		}
+		
+		file_ptr.close();
 		return T;
 	}
 		
-	int l_get_flight_time(Flight * ptr_Fl, Timetable * T) {
+extern "C" int l_get_flight_time(Flight * ptr_Fl, Timetable * T) {
 		return T->get_flight_time(T->get_flight_time(ptr_Fl->get_id())); //T - timetable
 	}
 
-	void print_timetable(Timetable * T) {
+extern "C" void print_timetable(Timetable * T) {
 		T->print();
 	}
-
-
-}
